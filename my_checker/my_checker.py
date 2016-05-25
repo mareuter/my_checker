7be2210b07510331f4c2289e1cb836d81fd46a5e
Michael Reuter <mareuternh@gmail.com>
2016-05-24 21:03:10 -0700

    Trying doc tests again.

--- my_checker/my_checker.py
+++ my_checker/my_checker.py
@@ -1,6 +1,14 @@
 def check_math(expression, answer):
     """Check a math expression with an answer.
 
+    >>> import my_checker
+    >>> if my_checker.check_math(6 * 9 - 12, 42):
+    ...     print("The answer!")
+    ... else:
+    ...     print("Sorry for the inconvenience.")
+    ...
+    The answer!
+
     Parameters
     ----------
     expression : math equation
@@ -11,6 +19,6 @@ def check_math(expression, answer):
     Returns
     -------
     bool
-        True if answer matches evaluated equation, False is not.
+        True if answer matches evaluated equation, False if not.
     """
     return expression == answer
